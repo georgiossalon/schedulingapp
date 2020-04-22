52a27689238025fa15a9f9d1c995afa3074a0d91
Felix Angelov <felangelov@gmail.com>
2020-04-21 22:33:13 -0500

    fix: make shiftsMap comparable by value

--- lib/blocs/shifts/shifts_state.dart
+++ lib/blocs/shifts/shifts_state.dart
@@ -12,15 +12,23 @@ abstract class ShiftsState extends Equatable {
 class ShiftsLoading extends ShiftsState {}
 
 class ShiftsLoaded extends ShiftsState {
-  final Map<DateTime,List<Shift>> shiftsMap;
+  final Map<DateTime, List<Shift>> shiftsMap;
 
-  const ShiftsLoaded([this.shiftsMap = const{}]);
+  const ShiftsLoaded([this.shiftsMap = const {}]);
 
   @override
-  List<Object> get props => [shiftsMap];
+  List<Object> get props => [_shiftsMap];
 
   @override
   String toString() => 'ShiftsLoaded {shifts: $shiftsMap }';
+
+  Map<String, List<Shift>> get _shiftsMap =>
+      Map<DateTime, List<Shift>>.from(shiftsMap).map(
+        (dateTime, shifts) => MapEntry<String, List<Shift>>(
+          dateTime.toIso8601String(),
+          shifts,
+        ),
+      );
 }
 
 class ShiftsNotLoaded extends ShiftsState {}
