ba86d6288d75fb79361d82fdb70d2c4d5eff452e
Salonikidis <georgios.salonikidis@outlook.com>
2020-04-22 15:09:18 +0200

    Implemented the calendar widget within main for simpler testing. Now it is possible to see the shifts under the calendar. The data shown is now realtime.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:bloc/bloc.dart';
 import 'package:snapshot_test/blocs/blocs.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shifts_repository/shifts_repository.dart';
-import 'package:snapshot_test/widgets/calendar.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   BlocSupervisor.delegate = SimpleBlocDelegate();
@@ -65,23 +65,24 @@ class ShiftListPage extends StatelessWidget {
             Map<DateTime,List<Shift>> map = ShiftsApp.shiftListToCalendarEventMap(shiftsList);
             print(map);
             //todo work with the map above to arange the shifts within the calendar
-            return Container(
-              child: Column(
-                children: <Widget>[
-                  //fixme: how to get the selected day?
-                  Calendar(),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: shiftsList.length,
-                      itemBuilder: (context, index) {
-                        final shift = shiftsList[index];
-                        // return _buildShiftContainer(shift: shift);
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            );
+            return Calendar(map: map,);
+            // Container(
+            //   child: Column(
+            //     children: <Widget>[
+            //       //fixme: how to get the selected day?
+            //       Calendar(),
+                  // Expanded(
+                  //   child: ListView.builder(
+                  //     itemCount: shiftsList.length,
+                  //     itemBuilder: (context, index) {
+                  //       final shift = shiftsList[index];
+                  //       // return _buildShiftContainer(shift: shift);
+                  //     },
+                  //   ),
+                  // ),
+            //     ],
+            //   ),
+            // );
           } else {
             return Container(
               child: Text('Shit happens'),
@@ -92,7 +93,67 @@ class ShiftListPage extends StatelessWidget {
     );
   }
 
-  Widget _buildShiftContainer({Shift shift}) {
+
+}
+
+
+class Calendar extends StatefulWidget {
+  Map<DateTime,List<Shift>> map;
+
+
+  Calendar({Key key, this.map}) : super(key: key);
+
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  // Example holidays
+final Map<DateTime, List> _holidays = {
+  DateTime(2019, 1, 1): ['New Year\'s Day'],
+  DateTime(2019, 1, 6): ['Epiphany'],
+  DateTime(2019, 2, 14): ['Valentine\'s Day'],
+  DateTime(2019, 4, 21): ['Easter Sunday'],
+  DateTime(2019, 4, 22): ['Easter Monday'],
+};
+  Map<DateTime, List> _events;
+  List _selectedEvents;
+  AnimationController _animationController;
+  CalendarController _calendarController;
+
+  @override
+  void initState() { 
+    super.initState();
+    // final _selectedDay = DateTime.now();
+    _calendarController = CalendarController();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _calendarController.dispose();
+    super.dispose();
+  }
+
+  void _onDaySelected(DateTime day, List events) {
+    print('CALLBACK: _onDaySelected');
+    setState(() {
+      if (_selectedEvents != null) {
+      _selectedEvents = events;
+
+      }
+    });
+  }
+
+  void _onVisibleDaysChanged(DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onVisibleDaysChanged');
+  }
+
+  void _onCalendarCreated(DateTime first, DateTime last, CalendarFormat format) {
+    print('CALLBACK: _onCalendarCreated');
+  }
+
+    Widget _buildShiftContainer({Shift shift}) {
 //    new Builder(builder: (gestureBuilder) {
     return Container(
 //      height: 250.0,
@@ -155,5 +216,119 @@ class ShiftListPage extends StatelessWidget {
       ),
     );
 //    });
+  }
+
+Widget _buildTableCalendarWithBuilders() {
+    return TableCalendar(
+      locale: 'de_DE',
+      events: widget.map,
+      initialCalendarFormat: CalendarFormat.twoWeeks,
+      calendarController: _calendarController,
+      calendarStyle: CalendarStyle(),
+      headerStyle: HeaderStyle(
+        centerHeaderTitle: true,
+        formatButtonDecoration: BoxDecoration(
+          color: Colors.pinkAccent.shade100,
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        formatButtonShowsNext: false,
+      ),
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      initialSelectedDay: DateTime.now(),
+//      initialSelectedDay: DateTime(2020, 3, 25),
+      onDaySelected: (date, events) {
+        setState(() {
+        // I use this for the length of the ListView.separator/builder
+          // _listOfShiftsPerGivenDay = events;
+        });
+      },
+      builders: CalendarBuilders(
+          selectedDayBuilder: (context, date, events) => Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(4.0),
+                color: Colors.pink,
+                child: Text(
+                  date.day.toString(),
+                ),
+              ),
+          todayDayBuilder: (context, date, events) => Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(4.0),
+                color: Colors.teal.shade100,
+                child: Text(
+                  date.day.toString(),
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                ),
+              ),
+          markersBuilder: (context, date, events, holidays) {
+            final children = <Widget>[];
+
+            if (events.isNotEmpty) {
+              children.add(
+                Positioned(
+                  right: 1,
+                  bottom: 1,
+                  child: _buildEventsMarker(date, events),
+                ),
+              );
+            }
+            return children;
+          }),
+    );
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: _calendarController.isSelected(date)
+            ? Colors.brown[500]
+            : _calendarController.isToday(date) ? Colors.brown[300] : Colors.blue[400],
+      ),
+      width: 16.0,
+      height: 16.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+   static DateTime utcTo12oclock(DateTime dateTimeToChange) {
+    if (dateTimeToChange != null) {
+      DateTime dateOfShift = dateTimeToChange.toLocal();
+      /*each device has a different utc. The table_calendar gives back
+    everything as utc. Thus I have to find a way to edit everything to 12 o'clock*/
+      return new DateTime(
+          dateOfShift.year, dateOfShift.month, dateOfShift.day, 12);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: Column(
+         children: <Widget>[
+           _buildTableCalendarWithBuilders(),
+           Expanded(
+                    child: ListView.builder(
+                      itemCount: widget.map[utcTo12oclock(_calendarController.selectedDay)] != null
+                                  ? widget.map[utcTo12oclock(_calendarController.selectedDay)].length
+                                  : 0,
+                      itemBuilder: (context, index) {
+                        final shift = widget.map[utcTo12oclock(_calendarController.selectedDay)][index];
+                       return _buildShiftContainer(shift: shift);
+                      },
+                    ),
+                  ),
+         ],
+       ),
+    );
   }
 }
