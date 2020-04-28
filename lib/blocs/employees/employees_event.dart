70b03e54470aa31e99f3e20ac26e9cb8fc8c0d44
Salonikidis <georgios.salonikidis@outlook.com>
2020-04-28 22:03:18 +0200

    Employees_Events done.

--- lib/blocs/employees/employees_event.dart
+++ lib/blocs/employees/employees_event.dart
@@ -22,6 +22,51 @@ class AddEmployee extends EmployeesEvent {
   String toString() => 'AddEmployee(employee: $employee)';
 }
 
+class UpdateEmployee extends EmployeesEvent {
+  final Employee updatedEmployee;
+
+  const UpdateEmployee(this.updatedEmployee);
+
+  @override
+  List<Object> get props => [updatedEmployee];
+
+  @override
+  String toString() => 'UpdateEmployee { updatedEmployee: $updatedEmployee }';
+}
+
+class RedoEmployee extends EmployeesEvent {
+  final Employee redoneEmployee;
+
+  const RedoEmployee(this.redoneEmployee);
+
+  @override
+  List<Object> get props => [redoneEmployee];
+
+  @override
+  String toString() => 'RedoEmployee { redoneEmployee: $redoneEmployee }';
+}
+
+class DeleteEmployee extends EmployeesEvent {
+  final Employee employee;
+
+  const DeleteEmployee(this.employee);
+
+  @override
+  List<Object> get props => [employee];
+
+  @override
+  String toString() => 'DeleteEmployee { employee: $employee }';
+}
+
+class EmployeesUpdated extends EmployeesEvent {
+  final List<Employee> employees;
+
+  const EmployeesUpdated(this.employees);
+
+  @override
+  List<Object> get props => [employees];
+}
+
 
 
 
